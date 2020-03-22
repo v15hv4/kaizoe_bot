e468d0f51dab1cba34aad670df8c8cce4f6be4e9
v15hv4 <vishva2912@gmail.com>
2020-03-22 15:33:09 +0530

    Switch to Parsel instead of Scrapy for covid_tracker

--- tg_bot/modules/covid_tracker.py
+++ tg_bot/modules/covid_tracker.py
@@ -3,40 +3,35 @@ from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
 from tg_bot import dispatcher
 
-import scrapy
-from scrapy.crawler import CrawlerProcess
+import requests
+from parsel import Selector
 
-selected = ''
-country = ''
-confirmed = 0
-deceased = 0
-recovered = 0
 
-class CovSpider(scrapy.Spider):
-    name = 'cov_spider'
-    start_urls = ['https://ncov2019.live/']
-    def parse(self, response):
-        global selected, country, confirmed, deceased, recovered
-        table = response.css('#sortable_table_Global')
-        rows = table.css('tr')
-        for row in rows:
-            country = row.css('.text--gray::text').getall()[0].strip()
-            if country.lower() == selected.lower():
-                confirmed = row.css('.text--green::text').getall()[0].strip()
-                deceased = row.css('.text--red::text').getall()[0].strip()
-                recovered = row.css('.text--blue::text').getall()[0].strip()
-                break
-
-cov_spider = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
-
+# TODO: Switch to Parsel instead of Scrapy
 def cov(bot: Bot, update: Update):
-    global selected, country, confirmed, deceased, recovered
+    country = ''
+    confirmed = 0
+    deceased = 0
+    recovered = 0
     message = update.effective_message
-    selected = message.text.split(' ')[1]
-    cov_spider.crawl(CovSpider)
-    cov_spider.start()
+    try:
+        selected = message.text.split(' ')[1]
+    except:
+        selected = 'TOTAL'
+    url = 'https://ncov2019.live/'
+    text = requests.get(url).text
+    selector = Selector(text = text)
+    table = selector.css('#sortable_table_Global')
+    rows = table.css('tr')
+    for row in rows:
+        country = row.css('.text--gray::text').getall()[0].strip()
+        if country.lower() == selected.lower():
+            confirmed = row.css('.text--green::text').getall()[0].strip()
+            deceased = row.css('.text--red::text').getall()[0].strip()
+            recovered = row.css('.text--blue::text').getall()[0].strip()
+            break
+        country = selected
+
     bot.send_message(
         message.chat.id,
         '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ ncov2019.live' % (country, confirmed, deceased, recovered),
