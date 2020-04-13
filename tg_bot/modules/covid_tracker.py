b36505751645bf209d8affc3709e8ca966d5f7b0
v15hv4 <vishva2912@gmail.com>
2020-04-13 21:59:19 +0530

    Switch to the Bing API for covid_tracker

--- tg_bot/modules/covid_tracker.py
+++ tg_bot/modules/covid_tracker.py
@@ -4,181 +4,107 @@ from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 from tg_bot import dispatcher
 from tg_bot import COVID_193_API_KEY
 
-import requests
-from parsel import Selector
-
 import os
 import json
+import requests
 from tabulate import tabulate
 from urllib.request import urlopen
 
 
 def cov(bot: Bot, update: Update):
     message = update.effective_message
-    d_title = ''
-    d_key = ''
-    country = ''
-    new = 0
     confirmed = 0
+    confirmed_delta = 0
     deceased = 0
+    deceased_delta = 0
     recovered = 0
-    m_rate = 0
-    r_rate = 0
-    country_input = ''.join([message.text.split(' ')[i] + ' ' for i in range(1, len(message.text.split(' ')))]).strip()
+    recovered_delta = 0
+    mortality_rate = 0
+    recovery_rate = 0
+    country_input = ''
+    state_input = ''
+    district_input = ''
 
-    if not country_input:
-        country_input = 'all'
-    
-    url_global = "https://covid-193.p.rapidapi.com/statistics"
-    headers = {
-        'x-rapidapi-host': "covid-193.p.rapidapi.com",
-        'x-rapidapi-key': COVID_193_API_KEY
-    }
-    json_response = requests.get(url_global, headers = headers)
-    global_dict = json.loads(json_response.text)
-    
-    if country_input.lower()[:3] == 'top':
-        if country_input.lower()[-1] == 'd':
-            d_title = 'DECEASED'
-            d_key = 'deaths'
-            d_subkey = 'total'
-        elif country_input.lower()[-1] == 'r':
-            d_title = 'RECOVERED'
-            d_key = 'cases'
-            d_subkey = 'recovered'
+    loc_input = message.text.split(',')
+    if len(loc_input) > 2:
+        district_input = loc_input[2].strip()
+    if len(loc_input) > 1:
+        state_input = loc_input[1].strip()
+    if len(loc_input) > 0:
+        country_input = loc_input[0][4:].strip()
+
+    try:
+        url_global = "https://bing.com/covid/data"
+        json_response = requests.get(url_global)
+        global_dict = json.loads(json_response.text)
+        
+        target = {}
+
+        if country_input:
+            for country in global_dict['areas']:
+                if country['displayName'].lower() == country_input.lower():
+                    if state_input:
+                        for state in country['areas']:
+                            if state['displayName'].lower() == state_input.lower():
+                                if district_input:
+                                    for district in state['areas']:
+                                        if district['displayName'].lower() == district_input.lower():
+                                            target = district
+                                else:
+                                    target = state
+                    else:
+                        target = country
         else:
-            d_title = 'CONFIRMED'
-            d_key = 'cases'
-            d_subkey = 'total'
-        try:
-            continent_list = [
-                'asia',
-                'north america',
-                'south america',
-                'oceania',
-                'europe',
-                'antarctica',
-                'africa'
-            ]
-            sorted_raw = sorted(global_dict['response'], key = lambda gdict: int(gdict[d_key][d_subkey]), reverse = True)[1:]   
-            sorted_list = [
-                sorted_raw[i] for i in range(len(sorted_raw)) 
-                if sorted_raw[i]['country'].replace('-', ' ').strip().lower() not in continent_list
-            ]
+            target = global_dict
 
-            n = country_input.lower()[3:].strip()
-            if n[-1].isalpha():
-                n = n[:-1]
-            n = int(n.strip()) + 1
-
-            country_list = []
-            for i in range(0, n):
-                country_list.append([
-                    str(i),
-                    sorted_list[i]['country'].replace('-', ' '),
-                    format(int(sorted_list[i][d_key][d_subkey]), ',d')
-                ])
-            out_list = str(tabulate(country_list, tablefmt = "plain"))
+        if not target:
             bot.send_message(
                 message.chat.id,
-                '`COVID-19 Tracker:` *%s*\n\n`%s`\n\n_Source:_ api-sports.io' % (d_title, out_list),
-                parse_mode = ParseMode.MARKDOWN,
-                disable_web_page_preview = True
-            )
-            return
-        except:
-            bot.send_message(
-                message.chat.id,
-                'Invalid argument!'
+                'Data unavailable for %s!' % (message.text[4:].strip())
             )
             return
 
+        confirmed = int(target['totalConfirmed'] or 0)
+        confirmed_delta = int(target['totalConfirmedDelta'] or 0)
+        deceased = int(target['totalDeaths'] or 0)
+        deceased_delta = int(target['totalDeathsDelta'] or 0)
+        recovered = int(target['totalRecovered'] or 0)
+        recovered_delta = int(target['totalRecoveredDelta'] or 0)
 
-    for gdict in global_dict['response']:
-        if gdict['country'].lower().replace('-', ' ') == country_input.lower():
-            new = int(gdict['cases']['new'][1:])
-            confirmed = int(gdict['cases']['total'])
-            deceased = int(gdict['deaths']['total'])
-            recovered = int(gdict['cases']['recovered'])
-            country = gdict['country'].replace('-', ' ')
-            m_rate = (100 * deceased / confirmed)
-            r_rate = (100 * recovered / confirmed)
-            break
+        mortality_rate = (deceased / confirmed) * 100
+        recovery_rate = (recovered / confirmed) * 100
 
-    if country.lower() == 'all':
-        country = 'global'
-    elif not country:
-        country = country_input
+        location = target['displayName'].upper()
 
-    bot.send_message(
-        message.chat.id,
-        '`COVID-19 Tracker:` *%s*\n\n' % country.upper() +
-        '*Confirmed:* %s   _(+%s in the past 24 hrs)_\n' % (format(confirmed, ',d'), format(new, ',d')) +
-        '*Deceased:* %s\n' % format(deceased, ',d') +
-        '*Recovered:* %s\n\n' % format(recovered, ',d') +
-        '*Mortality rate:* %s%%\n' % round(m_rate, 2) +
-        '*Recovery rate:* %s%%\n\n' % round(r_rate, 2) +
-        '_Source:_ api-sports.io',
-        parse_mode = ParseMode.MARKDOWN,
-        disable_web_page_preview = True
-    )
-
-
-def covindia(bot: Bot, update: Update):
-    message = update.effective_message
-    state = ''
-    new = 0
-    confirmed = 0
-    deceased = 0
-    recovered = 0
-    m_rate = 0
-    r_rate = 0
-    state_input = ''.join([message.text.split(' ')[i] + ' ' for i in range(1, len(message.text.split(' ')))]).strip()
-    if state_input:
-        url_india = 'https://api.covid19india.org/data.json'
-        json_url = urlopen(url_india)
-        state_dict = json.loads(json_url.read())
-        for sdict in state_dict['statewise']:
-            if sdict['state'].lower() == state_input.lower():
-                new = int(sdict['deltaconfirmed'])
-                confirmed = int(sdict['confirmed'])
-                deceased = int(sdict['deaths'])
-                recovered = int(sdict['recovered'])
-                state = sdict['state']
-                m_rate = (100 * deceased / confirmed)
-                r_rate = (100 * recovered / confirmed)
-                break
-    
-    if state:
         bot.send_message(
             message.chat.id,
-            '`COVID-19 Tracker:` *%s*\n\n' % state.upper() +
-            '*Confirmed:* %s   _(+%s in the past 24 hrs)_\n' % (format(confirmed, ',d'), format(new, ',d')) +
-            '*Deceased:* %s\n' % format(deceased, ',d') +
-            '*Recovered:* %s\n\n' % format(recovered, ',d') +
-            '*Mortality rate:* %s%%\n' % round(m_rate, 2) +
-            '*Recovery rate:* %s%%\n\n' % round(r_rate, 2) +
-            '_Source:_ covid19india.org',
+            '`COVID-19 Tracker:` *%s*\n\n' % location.upper() +
+            '*Confirmed:* %s _(+%s)_\n' % (format(confirmed, ',d'), format(confirmed_delta, ',d')) +
+            '*Deceased:* %s _(+%s)_\n' % (format(deceased, ',d'), format(deceased_delta, ',d')) +
+            '*Recovered:* %s _(+%s)_\n\n' % (format(recovered, ',d'), format(recovered_delta, ',d')) +
+            '*Mortality rate:* %s%%\n' % round(mortality_rate, 2) +
+            '*Recovery rate:* %s%%\n\n' % round(recovery_rate, 2) +
+            '[Powered by Bing.](https://bing.com/covid)',
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
-    else:
+        return
+
+    except:
         bot.send_message(
             message.chat.id,
-            'You need to specify a valid Indian state!'
+            'Unable to contact the Bing COVID-19 Data API. Try again in a while.'
         )
+        return
 
 
 __help__ = """
- - /cov <country>: Get real time COVID-19 stats for the input country
+ - /cov <country> <state> <locality>: Get real time COVID-19 stats for the input location.
  - /cov top <n(integer)>: Get the top n countries with the highest confirmed cases.
- - /covindia <state>: Get real time COVID-19 stats for the input Indian state
 """
 
 __mod_name__ = 'COVID-19 Tracker'
 
 COV_HANDLER = CommandHandler('cov', cov)
-COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
 
 dispatcher.add_handler(COV_HANDLER)
-dispatcher.add_handler(COV_INDIA_HANDLER)
