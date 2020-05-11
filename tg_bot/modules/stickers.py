4fd82ffe383225ccc3ac6cc7892868daa5b606cd
v15hv4 <vishva2912@gmail.com>
2020-05-12 04:07:21 +0530

    stickers: Made text messages stickerizable

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -5,7 +5,7 @@ from telegram.error import RetryAfter
 import os
 import urllib.request
 from math import ceil
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 from tg_bot import dispatcher
 from tg_bot import UNIV_STICKER_OWNER_ID, ATAF_FONT_URL
@@ -43,6 +43,84 @@ def add_sticker(bot: Bot, update: Update):
         else:
             sticker.thumbnail((512, 512))
         sticker.save('sticker_input.png')
+    elif message.reply_to_message.text:
+        target_text = message.reply_to_message.text
+        target_from = message.reply_to_message.from_user
+        target_name = target_from.first_name
+        target_file = str(message.reply_to_message.message_id) + '.png'
+        target_ppic = target_from.get_profile_photos().photos[0][0].get_file()
+        target_ppic.download(target_file)
+        parts = []
+        padding = 100
+        linelen = 0
+        offset = 20
+        nrows = 1
+        ncols = 1
+        line = ''
+        for word in target_text.split(' '):
+            linelen += len(word)
+            if linelen < 35:
+                line = line + word + ' '
+                if linelen > ncols:
+                    ncols = linelen
+            else:
+                parts.append(line.strip())
+                linelen = 0
+                line = word + ' '
+        parts.append(line)
+        nrows = len(parts)
+        if len(target_name) > ncols:
+            ncols = len(target_name)
+
+        IMG_WIDTH = ncols * 11 + padding + 30
+        IMG_HEIGHT = (nrows + 1) * 20 + padding - 40
+        BUBBLE_WIDTH = ncols * 11 + padding + 20
+        BUBBLE_HEIGHT = (nrows + 1) * 20 + padding - 50
+        corner = Image.new('RGB', (10, 10), (0, 0, 0))
+        drawcorner = ImageDraw.Draw(corner)
+        drawcorner.pieslice((0, 0, 20, 20), 180, 270, fill='#212121')
+        bubble = Image.new('RGB', (IMG_WIDTH, IMG_HEIGHT), (0, 0, 0))
+        fnt_name = ImageFont.truetype('Roboto-Medium.ttf', 18)
+        fnt_msg = ImageFont.truetype('Roboto-Regular.ttf', 18)
+        draw = ImageDraw.Draw(bubble)
+        draw.rectangle(((60, 10), (BUBBLE_WIDTH, BUBBLE_HEIGHT)), fill='#212121')
+        bubble.paste(corner, (60, 10))
+        bubble.paste(corner.rotate(90), (60, BUBBLE_HEIGHT - 9))
+        bubble.paste(corner.rotate(180), (BUBBLE_WIDTH - 9, BUBBLE_HEIGHT - 9))
+        bubble.paste(corner.rotate(270), (BUBBLE_WIDTH - 9, 10))
+        draw.text((75, 22), target_name, font = fnt_name)
+        for idx, inp in enumerate(parts):
+            draw.text((75, 48 + (offset * idx)), inp, font = fnt_msg)
+        ppic = Image.open(target_file)
+        ppic.resize((40, 40))
+        bigsize = (ppic.size[0] * 3, ppic.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        drawmask = ImageDraw.Draw(mask)
+        drawmask.ellipse((0, 0) + bigsize, fill = 255)
+        mask = mask.resize(ppic.size, Image.ANTIALIAS)
+        ppic.putalpha(mask)
+        ppic_circ = ImageOps.fit(ppic, mask.size, centering = (0.5, 0.5))
+        ppic_circ.putalpha(mask)
+        ppic_circ.thumbnail((36, 36))
+        bubble.paste(ppic_circ, (10, IMG_HEIGHT - 50))
+        if bubble.width < 512 and bubble.height < 512:
+            if bubble.width < bubble.height:
+                asp_ratio = bubble.width / bubble.height
+                bubble = bubble.resize((ceil(asp_ratio * 512), 512))
+            else:
+                asp_ratio = bubble.height / bubble.width
+                bubble = bubble.resize((512, ceil(asp_ratio * 512)))
+        else:
+            bubble.thumbnail((512, 512))
+        bubble.save(str(message.message_id) + '.webp')
+        bot.send_document(
+            message.chat.id,
+            document = open(str(message.message_id) + '.webp', 'rb'),
+            reply_to_message_id = message.message_id
+        )
+        os.remove(target_file)
+        os.remove(str(message.message_id) + '.webp')
+        return
     else:
         message.reply_text('bruh')
         return
