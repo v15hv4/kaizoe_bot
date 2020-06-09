ea354601af12f2cf66646fe21931d16776ef13e6
v15hv4 <vishva2912@gmail.com>
2020-06-10 02:59:10 +0530

    github: Pass repo and entry count as args

--- tg_bot/modules/github.py
+++ tg_bot/modules/github.py
@@ -10,20 +10,23 @@ from github import Github
 
 
 @run_async
-def commits(repo, bot, update):
+def commits(repo, entries, bot, update):
     message = update.effective_message
-    commits = [
-        "[commit %s](%s)\n*Author:* %s `<%s>`\n*Date:* %s\n\n\t%s\n\n"
-        % (
-            commit.commit.sha[:20],
-            commit.commit.html_url,
-            commit.commit.author.name,
-            commit.commit.author.email,
-            commit.commit.author.date,
-            commit.commit.message,
+    commits = []
+    for count, commit in enumerate(repo.get_commits()):
+        if count >= entries:
+            break
+        commits.append(
+            "[commit %s](%s)\n*Author:* %s `<%s>`\n*Date:* %s\n\n\t%s\n\n"
+            % (
+                commit.commit.sha[:20],
+                commit.commit.html_url,
+                commit.commit.author.name,
+                commit.commit.author.email,
+                commit.commit.author.date,
+                commit.commit.message,
+            )
         )
-        for commit in repo.get_commits()
-    ]
     message.reply_text(
         "\n".join(commits), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
     )
@@ -63,15 +66,26 @@ def events(repo, bot, update):
 
 @run_async
 def gh(bot: Bot, update: Update, args: List[str]):
-    arg = args[0]
-    g = Github(GH_AUTH_TOKEN)
-    repo = g.get_repo("v15hv4/dotfiles")
-    if arg.lower() == "commits" or arg.lower() == "log":
-        commits(repo, bot, update)
-    elif arg.lower() == "events":
-        events(repo, bot, update)
-    else:
-        update.effective_message.reply_text("Invalid command!")
+    try:
+        command = args[0].lower()
+        if len(args) < 2:
+            update.effective_message.reply_text("Target repository not specified!")
+        repo = args[1].lower()
+        g = Github(GH_AUTH_TOKEN)
+        target = g.get_repo(repo)
+        if command == "commits" or command == "log":
+            entries = 5
+            if len(args) > 2:
+                entries = int(args[2])
+            commits(target, entries, bot, update)
+        elif command == "events":
+            events(target, bot, update)
+        else:
+            update.effective_message.reply_text("'%s' is not a valid gh command." % args[0])
+    except:
+        update.effective_message.reply_text(
+            "Something went wrong! Check command arguments and retry in a while."
+        )
 
 
 GH_HANDLER = CommandHandler("gh", gh, pass_args=True)
