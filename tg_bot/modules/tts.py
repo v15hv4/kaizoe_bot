7a1fb33b6d6c8fe9e2a729d6b97ab12b32585ed4
v15hv4 <vishva2912@gmail.com>
2020-07-16 11:27:01 +0530

    tts: Introduce 'say' command

---
+++ tg_bot/modules/tts.py
@@ -0,0 +1,27 @@
+from telegram import ParseMode, Update, Bot, Chat
+from telegram.ext import CommandHandler, run_async
+
+from tg_bot import dispatcher
+
+import os
+from gtts import gTTS
+
+
+@run_async
+def say(bot: Bot, update: Update):
+    message = update.effective_message
+    text = " ".join(message.text.split(" ")[1:])
+    tts_id = "tts_%s_%s.mp3" % (str(message.chat.id), str(message.message_id))
+    tts = gTTS(text, lang="en-us")
+    tts.save(tts_id)
+    bot.send_audio(
+        message.chat.id, audio=open(tts_id, "rb"), reply_to_message_id=message.message_id
+    )
+    os.remove(tts_id)
+
+
+__mod_name__ = "TTS"
+
+SAY_HANDLER = CommandHandler("say", say)
+
+dispatcher.add_handler(SAY_HANDLER)
