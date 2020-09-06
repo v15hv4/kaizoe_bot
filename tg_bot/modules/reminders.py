9e83892bb03a753bf3e77e3621cf81c05eb6c383
v15hv4 <vishva2912@gmail.com>
2020-09-07 03:15:17 +0530

    reminders: Introduce remindme command

---
+++ tg_bot/modules/reminders.py
@@ -0,0 +1,28 @@
+from telegram import ParseMode, Update, Bot, Chat
+from telegram.ext import CommandHandler, run_async
+
+from tg_bot import dispatcher, updater
+
+from typing import List
+
+job = updater.job_queue
+
+
+def push_reminder(bot, job):
+    job.context["message"].reply_text("sched message")
+
+
+@run_async
+def remindme(bot, update, args: List[str]):
+    global job
+    context = {"message": update.effective_message, "args": args}
+    print(args)
+    job_remindme = job.run_once(push_reminder, 30, context=context)
+
+
+__mod_name__ = "Reminders"
+
+
+REMINDME_HANDLER = CommandHandler("remindme", remindme, pass_args=True)
+
+dispatcher.add_handler(REMINDME_HANDLER)
