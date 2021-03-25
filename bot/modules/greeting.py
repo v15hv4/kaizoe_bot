1865f96d8aebfc435ab6783617940fb2d5a0233f
v15hv4 <vishva2912@gmail.com>
2021-03-25 18:44:25 +0530

    Fix command prefix regex and add sample methods

--- bot/modules/greeting.py
+++ bot/modules/greeting.py
@@ -1,6 +1,13 @@
-from bot import CLIENT as client
+from pyrogram import filters
+from bot import CLIENT as client, COMMAND_PREFIX as prefix
 
 
-@client.on_message()
-async def dothis(_, message):
-    await message.reply_text("sup")
+@client.on_message(filters.regex(f"^(?i)((hey|hi|hello|sup) kaizoe)"))
+async def greeting(_, message):
+    await message.reply_text(f"Hey {message.from_user.first_name}! Sup?")
+
+
+@client.on_message(filters.regex(f"^{prefix}.*"))
+async def handle_command(_, message):
+    command, *args = message.text.split(" ")
+    await message.reply_text(f"The {command} command with arguments {str(args)} is undefined.")
